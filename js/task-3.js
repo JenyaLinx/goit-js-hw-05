0db28874f7de2f45c66a0f09cf8c04af01689298
Jenya Linx <sportpit2020@gmail.com>
2025-08-09 20:00:25 +0300

    second fly

--- js/task-3.js
+++ js/task-3.js
@@ -73,7 +73,8 @@
 // console.log(profile.getInfo()); // "Marco has 320 active hours!"
 
 
-const sortByDescendingFriendCount = (users) => [...users].sort((a, b) => b.friends.length - a.friends.length);
+const sortByDescendingFriendCount = (users) =>
+  users.toSorted((a, b) => b.friends.length - a.friends.length);
 
 console.log(
   sortByDescendingFriendCount([
